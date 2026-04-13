a6a49ea1042790c01b959ae5dbcce97d0120257f
Santiago Niño <a01665906@tec.mx>
2026-04-13 01:31:25 -0600

    Desarrollo de componente generico Modal, listo para ser reutilizado en diferentes partes de la aplicacion, como generar elementos de dashboard, agregar proyeccion, mas formularios, etc. Muy interesante el uso de un barrel export para exportar los modales de su propio directorio, habre de preguntarle al profe si es una buena practica. Hice un pequeño test para renderizar este modal con un boton haciendo uso del useState, y vemos que el modal se despliega bien, bloqueando acciones externas a este, cerrandolo con su X o ESC, la logica de isOpen y teniendo los props de content adecuados para su reutilizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+import Modal from './components/common/Modal'
+
 
 function App() {
-
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className='bg-blue-500 text-white p-8 text-2x1 rounded-x1'>
-      <p>Hello World! Iniciamos el proyecto HealthInsights!!</p>
+    <div className="p-8">
+      <p>Hello World HealthInsights!</p>
+      
+      <button onClick={() => setIsOpen(true)}>
+        Abrir Modal
+      </button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        title="Título del Modal"
+        subtitle="Subtítulo opcional"
+      >
+        <p>Contenido del modal aquí. Estamos probando</p>
+      </Modal>
     </div>
   )
 }
