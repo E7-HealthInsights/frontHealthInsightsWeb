61acddff8fa088cb3cac76e933a38ec4c5d8f3e1
Santiago Niño <a01665906@tec.mx>
2026-04-12 23:31:31 -0600

    Instalacion de TailwindCSS con npm install -D @tailwindcss/vite

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
 
 
   return (
-    <>
+    <div className='bg-blue-500 text-white p-8 text-2x1 rounded-x1'>
       <p>Hello World! Iniciamos el proyecto HealthInsights!!</p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/common/Modal/index.ts
@@ -0,0 +1,2 @@
+// Barrel export - exports all components from the Modal directory
+// para que el import sea mas limpio y organizado
