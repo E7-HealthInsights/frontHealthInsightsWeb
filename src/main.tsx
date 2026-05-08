7c8ead66c06135d4eb99140cd3a6f0a961bfd820
Santiago Niño <a01665906@tec.mx>
2026-05-08 17:33:24 -0600

    Render de la pagina de Dashboard, haciendo uso del contexto del QueryClientProvider en main, y en la page solo renderiza cada widget segun lo quevenga del backend, asi ya tenemos elementos en el dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/reactQuery.ts'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
